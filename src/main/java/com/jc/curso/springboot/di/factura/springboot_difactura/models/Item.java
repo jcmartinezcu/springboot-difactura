f129d129d799573e8eaa3b1528d508e3175b67b0
jcmartinez <jcmartinezcu@gmail.com>
2024-06-09 13:37:55 +0200

    Factura y relaciones entre sus componentes

--- src/main/java/com/jc/curso/springboot/di/factura/springboot_difactura/models/Item.java
+++ src/main/java/com/jc/curso/springboot/di/factura/springboot_difactura/models/Item.java
@@ -2,4 +2,22 @@ package com.jc.curso.springboot.di.factura.springboot_difactura.models;
 
 public class Item {
 
+    private Product product;
+    private Integer quantity;
+    
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    
+
 }
