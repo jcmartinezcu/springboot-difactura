25a6b152c22ac519827d1d915b3d07f39e2d3995
jcmartinez <jcmartinezcu@gmail.com>
2024-06-09 13:53:30 +0200

    Registrando componente Items de la factura

--- src/main/java/com/jc/curso/springboot/di/factura/springboot_difactura/AppConfig.java
+++ src/main/java/com/jc/curso/springboot/di/factura/springboot_difactura/AppConfig.java
@@ -1,10 +1,26 @@
 package com.jc.curso.springboot.di.factura.springboot_difactura;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import com.jc.curso.springboot.di.factura.springboot_difactura.models.Item;
+import com.jc.curso.springboot.di.factura.springboot_difactura.models.Product;
 
 @Configuration
 @PropertySource("classpath:data.properties")
 public class AppConfig {
+
+    @Bean
+    List<Item> itemsInvoice(){
+        Product p1 = new Product("Camara Sony",800);
+        Product p2 = new Product("Biciclete Bianchi 26",1200);
+        
+        return Arrays.asList(new Item(p1,2), new Item(p2,4));
+
+    }
 
 }
