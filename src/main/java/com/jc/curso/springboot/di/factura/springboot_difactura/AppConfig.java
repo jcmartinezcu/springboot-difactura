1090f18ca982bf751aa864fc665df14d3010f6c2
jcmartinez <jcmartinezcu@gmail.com>
2024-06-21 17:23:31 +0200

    registrando otras lineas de factura

--- src/main/java/com/jc/curso/springboot/di/factura/springboot_difactura/AppConfig.java
+++ src/main/java/com/jc/curso/springboot/di/factura/springboot_difactura/AppConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 import com.jc.curso.springboot.di.factura.springboot_difactura.models.Item;
@@ -14,12 +15,24 @@ import com.jc.curso.springboot.di.factura.springboot_difactura.models.Product;
 @PropertySource(value = "classpath:data.properties", encoding = "UTF-8")
 public class AppConfig {
 
-    @Bean
+    @Bean("default")
+    // @Primary
     List<Item> itemsInvoice(){
         Product p1 = new Product("Camara Sony",800);
         Product p2 = new Product("Biciclete Bianchi 26",1200);
         
         return Arrays.asList(new Item(p1,2), new Item(p2,4));
+
+    }
+    @Bean
+    List<Item> itemsInvoiceOfffice(){
+        Product p1 = new Product("Monitor Asus 24",700);
+        Product p2 = new Product("Notebook Razer",2400);
+        Product p3 = new Product("Impresora HP",800);
+        Product p4 = new Product("Escritorio Oficina",900);
+        
+        return Arrays.asList(new Item(p1,4), new Item(p2,6),
+                             new Item(p3,1), new Item(p4,4));
 
     }
 
